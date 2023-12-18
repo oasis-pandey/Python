ec6a9e679f85a142e1d3b6981402e1fd3b6815b2
hngrmn <143054580+hngrmn@users.noreply.github.com>
2023-12-18 10:54:43 +0545

    comments on strings

--- Strings/removing strips.py
+++ Strings/removing strips.py
@@ -2,3 +2,6 @@ name=" \t\n oasis  "
 print(name)
 name=name.lstrip()
 print(name)
+#You can use lstrip to clear spaces(strips) in the left,
+# rstrip to clear the right, 
+# and "strip" to clear both at a time
