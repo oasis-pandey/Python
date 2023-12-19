913ae7c2e6234e28a82551152afd234a3eefed73
hngrmn <143054580+hngrmn@users.noreply.github.com>
2023-12-19 14:40:35 +0545

    added comment

--- Strings/Personal message to a name.py
+++ Strings/Personal message to a name.py
@@ -1,3 +1,5 @@
 #-------------------------------------------------------------------------------
 name="oasis"
 print("Hello "+name.title()+", "+"Would you like to learn some python today?")
+
+# /n is used to create a new line. 
