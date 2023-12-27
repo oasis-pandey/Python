10c0ddd20095995bffac1bd449c655c05964dbb8
hngrmn <143054580+hngrmn@users.noreply.github.com>
2023-12-27 18:04:52 +0545

    list comprehension

--- Lists/first 10 square numbers.py
+++ Lists/first 10 square numbers.py
@@ -5,6 +5,10 @@ for i in range(1,11):
     value= i**2 # it takes numbers from 1 to 10 and (**2 i.e squares them)
     number.append(value) # it adds the squared number to the list
 
+## first 10 square numbers using list comprehensions:
+squares= [value**2 for value in range(1,11)]
+print(squares)
+
 print(number) # it prints the first 10 square numbers
 
 #simple statistics in python
