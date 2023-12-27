d02a7e0c792b1238bf62672a68a91630add392be
hngrmn <143054580+hngrmn@users.noreply.github.com>
2023-12-27 22:47:24 +0545

    list slice

---
+++ Lists/list slice.py
@@ -0,0 +1,6 @@
+# to print specific items in list
+letters=['A','B','C','D','E','F']
+print(letters[0:4]) # it prints from 0 to 3 i.e a,b,c,d
+print(letters[2:4]) # prints from 2 and ends at D
+print(letters[:3])# starts from 0 and prints till 2nd item i.e C
+print(letters[3:])#starts from 3 and ends all the way to the end
