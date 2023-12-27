6145048721d5758325b2045ecfc199b02a32e986
hngrmn <143054580+hngrmn@users.noreply.github.com>
2023-12-27 16:10:32 +0545

    added comments

--- Lists/first 10 square numbers.py
+++ Lists/first 10 square numbers.py
@@ -1,8 +1,8 @@
 # we are going to work on printing first 10 square numbers
 
-number=[]
+number=[] # we start by introducing an empty list
 for i in range(1,11):
-    value= i**2
-    number.append(value)
+    value= i**2 # it takes numbers from 1 to 10 and (**2 i.e squares them)
+    number.append(value) # it adds the squared number to the list
 
-print(number)
+print(number) # it prints the first 10 square numbers
