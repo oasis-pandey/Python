bc9c512d54c89e715f634518038b2f3ec8a028e9
hngrmn <143054580+hngrmn@users.noreply.github.com>
2023-12-27 20:39:22 +0545

    organized exercises of lists

---
+++ Lists/printing a million numbers.py
@@ -0,0 +1,12 @@
+'''
+4-4. One Million: Make a list of the numbers from one to one million, and then
+use a for loop to print the numbers. (If the output is taking too long, stop it by
+pressing ctrl-C or by closing the output window.
+
+''' 
+
+
+million=list(range(1,1000001))
+for i in million:
+    print(i)
+
