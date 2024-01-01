d4117378f070d0f2dc897b104d45df8feef2f3a6
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-01 20:58:08 +0545

    checking items in a list

--- Conditionals/present in list or not.py
+++ Conditionals/present in list or not.py
@@ -1,4 +1,4 @@
 # here we are going to check if an item is present in a list
 alphabets=['a','b','c','d']
 print('a' in alphabets) # "in" command checks if its present or not
-print('f' not in alphabets)
+print('f' not in alphabets) # "not in " tells us if its present 
