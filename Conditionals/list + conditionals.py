ef63ca3cefbeda392b363f92f9f9cf1c8a8b7c17
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-02 23:01:18 +0545

    checking is one item is in another list

--- Conditionals/list + conditionals.py
+++ Conditionals/list + conditionals.py
@@ -14,14 +14,24 @@
 
 
 
-usernames=[]
-if usernames:
-    print("we have some users")
-else:
-    print("we need to find some users")
+# usernames=[]
+# if usernames:
+#     print("we have some users")
+# else:
+#     print("we need to find some users")
 
 
+current_users=['a','b','c','d','e','f']
+current_users_lower=[current_user.lower() for current_user in current_users]
+new_users=['g','a','i','f','k','l']
+new_users_lower=[new_user.lower() for new_user in new_users]
 
+
+for new_user in new_users_lower:
+    if new_user in current_users_lower:
+        print("enter a new username")
+    else:
+        print("username available")
 
     
 
