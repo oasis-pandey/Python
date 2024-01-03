925e01f8624818e9077eca2ca32514e74b41dff0
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-03 06:41:05 +0545

    simple dictionaries

---
+++ Dictionaries/dictionaries.py
@@ -0,0 +1,8 @@
+aliens={'color':'green','points':5}
+print(aliens['color'])
+aliens['X dimension']=10
+aliens['Y dimension']=15
+print(aliens.keys())
+print(aliens.values())
+print(aliens.items())
+print(aliens)
