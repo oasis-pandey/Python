3d674f0cd2bcb0cd1304fb2fcb7dabf13c458f54
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-03 16:35:18 +0545

    favorite numbers

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -7,19 +7,45 @@
 # print(aliens.items())
 # print(aliens)
 # print(aliens['X dimension'])
+# del aliens['color']    deletes an item in dictionary 
+# print(aliens)
 
 
 # aliens['color']='yellow'
 # print(f"the color of the alien is now {aliens['color']}")
 
-alien_0={'speed':'medium','X position':0,'Y position':0}
-if alien_0['speed']=='slow':
-    X_increment=1
-elif alien_0['speed']=='medium':
-    X_increment=2
-else:
-    X_increment=3
+# alien_0={'speed':'medium','X position':0,'Y position':0}
+# if alien_0['speed']=='slow':
+#     X_increment=1
+# elif alien_0['speed']=='medium':
+#     X_increment=2
+# else:
+#     X_increment=3
 
-new_position=alien_0['X position']+(X_increment)
+# new_position=alien_0['X position']+(X_increment)
 
-print("the new position of X is: "+ str(new_position))
+# print("the new position of X is: "+ str(new_position))
+
+# alien={
+#     'hi':'hello'
+# }
+# print(alien['hi'])
+
+# person={
+#     'first_name':'oasis',
+#     'last_name':'pandey',
+#     'city':'kathmandu'    
+# }
+# print(person['first_name'])
+
+favorite_numbers={
+    'manish':18,
+    'anuj':10,
+    'oasis':17,
+    'anoodit': 5,
+    'geda':9
+}
+for people, number in favorite_numbers.items():
+        print("the favorite number of "+ people+ " is " +str(number))
+
+
