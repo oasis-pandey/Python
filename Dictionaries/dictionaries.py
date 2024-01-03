74f345baad6c52c38d7e13b6c9068eb39dc21e43
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-03 19:35:57 +0545

    for loop in dictionary

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -61,21 +61,7 @@
 # for keys, meanings in glossary.items():
 #     print(keys+ ":"+ meanings+"\n")
 
-# favorite_languages = {
-#  'jen': 'python',
-#  'sarah': 'c',
-#  'edward': 'ruby',
-#  'phil': 'python',
-#  }
 
-# friends = ['phil', 'sarah']
-
-# for names in favorite_languages.keys():
-#     print(names.title())
-
-# for name in friends:
-#     if name in favorite_languages:
-#         print("Hello, "+name+" Glad to hear that your favorite language is "+favorite_languages[name].title()+"!")
 
     
 
---
+++ Dictionaries/words and meanings.py
@@ -0,0 +1,10 @@
+glossary={
+    'a':'apple',
+    'b':'ball',
+    'c':'cat',
+    'd':'dog'
+
+}
+for keys, meanings in glossary.items():
+    print(keys+ ":"+ meanings+"\n")
+
