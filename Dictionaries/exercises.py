e3a6052429170079bc3acec4c4bad1b8cc6211de
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-03 21:40:54 +0545

    inviting or thanking for vote

--- Dictionaries/exercises.py
+++ Dictionaries/exercises.py
@@ -1,11 +1,13 @@
-rivers={
-    'nile':'egypt',
-    'amazon':'brazil',
-    'karnali':'nepal'
-}
-for river, country in rivers.items():
-    print("The "+ river.title() +" river runs through "+country.title()+".")
+# rivers={
+#     'nile':'egypt',
+#     'amazon':'brazil',
+#     'karnali':'nepal'
+# }
+# for river, country in rivers.items():
+#     print("The "+ river.title() +" river runs through "+country.title()+".")
 
-for river, country in rivers.items():
-    print(river)
-    print(country)
+# for river, country in rivers.items():
+#     print(river)
+#     print(country)
+
+
