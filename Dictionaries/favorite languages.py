2cb557b436051ea3fc32d002e61a765f42c462fa
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-03 20:42:03 +0545

    sorted dictionary

--- Dictionaries/favorite languages.py
+++ Dictionaries/favorite languages.py
@@ -4,12 +4,14 @@ favorite_languages = {
  'edward': 'ruby',
  'phil': 'python',
  }
+for name, language in (favorite_languages.items()):
+    print(name)
 
-friends = ['phil', 'sarah']
+# friends = ['phil', 'sarah']
 
-for names in favorite_languages.keys():
-    print(names.title())
+# for names in favorite_languages.keys():
+#     print(names.title())
 
-for name in friends:
-    if name in favorite_languages:
-        print("Hello, "+name+" Glad to hear that your favorite language is "+favorite_languages[name].title()+"!")
+# for name in friends:
+#     if name in favorite_languages:
+#         print("Hello, "+name+" Glad to hear that your favorite language is "+favorite_languages[name].title()+"!")
