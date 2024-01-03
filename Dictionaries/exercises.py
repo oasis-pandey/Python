b0c4c2c24d2cb2b838595b7ed885a2334888f7cd
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-03 21:20:39 +0545

    loops exercise in dictionary

---
+++ Dictionaries/exercises.py
@@ -0,0 +1,11 @@
+rivers={
+    'nile':'egypt',
+    'amazon':'brazil',
+    'karnali':'nepal'
+}
+for river, country in rivers.items():
+    print("The "+ river.title() +" river runs through "+country.title()+".")
+
+for river, country in rivers.items():
+    print(river)
+    print(country)
