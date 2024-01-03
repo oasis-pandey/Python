c9b69f7dfa5c75ac1cd7543fcf4994daa41f226b
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-03 06:57:03 +0545

    if elif in dictionaries

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -1,13 +1,25 @@
-aliens={'color':'green','points':5}
-print(aliens['color'])
-aliens['X dimension']=10
-aliens['Y dimension']=15
-print(aliens.keys())
-print(aliens.values())
-print(aliens.items())
-print(aliens)
-print(aliens['X dimension'])
+# aliens={'color':'green','points':5}
+# print(aliens['color'])
+# aliens['X dimension']=10
+# aliens['Y dimension']=15
+# print(aliens.keys())
+# print(aliens.values())
+# print(aliens.items())
+# print(aliens)
+# print(aliens['X dimension'])
 
 
-aliens['color']='yellow'
-print(f"the color of the alien is now {aliens['color']}")
+# aliens['color']='yellow'
+# print(f"the color of the alien is now {aliens['color']}")
+
+alien_0={'speed':'medium','X position':0,'Y position':0}
+if alien_0['speed']=='slow':
+    X_increment=1
+elif alien_0['speed']=='medium':
+    X_increment=2
+else:
+    X_increment=3
+
+new_position=alien_0['X position']+(X_increment)
+
+print("the new position of X is: "+ str(new_position))
