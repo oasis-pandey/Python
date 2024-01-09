d384e370406bd6ab4a7ea3343efd9f7a870a1767
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-09 21:48:18 +0545

    very important methods in dictionary

--- Dictionaries/exercises.py
+++ Dictionaries/exercises.py
@@ -1,13 +1,22 @@
-# rivers={
-#     'nile':'egypt',
-#     'amazon':'brazil',
-#     'karnali':'nepal'
-# }
+rivers={
+    'nile':'egypt',
+    'amazon':'brazil',
+    'karnali':'nepal'
+}
 # for river, country in rivers.items():
 #     print("The "+ river.title() +" river runs through "+country.title()+".")
 
 # for river, country in rivers.items():
 #     print(river)
 #     print(country)
-
+# print(rivers.get('nile'))
+rivers.update({'ram':'shyam'})
+print(rivers)
+print(rivers.items())
+rivers.pop('amazon') # DELETE A VALUE
+print(rivers.get('ram')) # ACCESS A CERTAIN VALUE
+rivers.popitem()# DELETES THE LAST VALUE 
+print(rivers)
+rivers.clear()# DELETES EVERYTHING
+print(rivers)
 
