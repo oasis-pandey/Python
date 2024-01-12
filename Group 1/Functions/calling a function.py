3280955baf87b59a15c28e55578e6733f33e83be
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-12 15:37:51 +0545

    tshirt size exercise

--- Group 1/Functions/calling a function.py
+++ Group 1/Functions/calling a function.py
@@ -1,4 +1,9 @@
-def full_name(first_name, last_name):
-    return ("Hello, " + first_name +" "+ last_name  +" How are you today? ")
-greeting= full_name('Oasis', 'Pandey')
-print(greeting)
+# def full_name(first_name, last_name):
+#     return ("Hello, " + first_name +" "+ last_name  +" How are you today? ")
+# greeting= full_name('Oasis', 'Pandey')
+# print(greeting)
+
+def make_shirt(size, text):
+    return(f'You tshirt will be of size {size} and the text that will be printed is {text}')
+c= make_shirt(8,'"Hello World!"')
+print(c)
