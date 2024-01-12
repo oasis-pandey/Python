374fc31f7ea4add843cb51314df7d15fc90fea09
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-12 15:45:03 +0545

    default parameters

--- Group 1/Functions/calling a function.py
+++ Group 1/Functions/calling a function.py
@@ -3,7 +3,13 @@
 # greeting= full_name('Oasis', 'Pandey')
 # print(greeting)
 
-def make_shirt(size, text):
-    return(f'You tshirt will be of size {size} and the text that will be printed is {text}')
-c= make_shirt(8,'"Hello World!"')
-print(c)
+# def make_shirt(size="large", text='"I love python."'):
+#     return(f'You tshirt is {size} size and the text that will be printed is {text}')
+# c= make_shirt(size="medium")
+# print(c)
+
+def describe_city(city,country='Nepal'):
+    return(f"{city} lies in {country}")
+message= describe_city("Kathmandu","Nepal")
+print(message)
+
