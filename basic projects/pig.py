7f865b0890c26b5dfa424dba0c4598d4bfeb29bb
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-01-27 00:18:08 +0545

    completed

--- basic projects/pig.py
+++ basic projects/pig.py
@@ -1,14 +1,16 @@
 import random
-
+score=0
+#creating a roll funtion to roll the dice.
 while True:
     def roll():
         x=random.randint(0,6)
         return x
     
-    score=0
 
     players=input("Enter the number of players:(2-4) ")
+    player_scores=[0 for i in range(int(players))]    
 
+#ensuring that the number entered is between 2-4 and is a valid number.
     if players.isdigit():
         players=int(players)
         if players>=2 and players<=4:
@@ -18,25 +20,32 @@ while True:
     else:
         print("Enter a number!")
 
-while True:
-    
-    roll_or_not=input("Do you want to roll the dice?(y) ")
+#Rolling.
+for player_index in range(int(players)):
+    while True:
+        print("PLAYER", str(player_index+1),"TURN!")
+        roll_or_not=input("Do you want to roll the dice?(y) ")
 
-    if roll_or_not=='y':
-        value=roll()
+        if roll_or_not=='y':
+            value=roll()
+            
+            
+            if value==1:
+                print("Your rolled a 1. Turn Over!")
+                score=0
+                print("Your score is 0.")
+                break
+            else:
+                print("You rolled a "+str(value))
+                score+=value
+                print("your score is",score)
+                player_scores[player_index]=score
         
-        if value==1:
-            print("Your rolled a 1. Turn Over!")
-            score=0
-            print("Your score is 0.")
         else:
-            print("You rolled a "+str(value))
-            score+=value
-            print("your score is",score)
-        
-    else:
-        break
-
+            break
+x=player_scores.index(max(player_scores))
+x=x+1
+print("PLAYER"+str(x)+" WON with a total score of: "+str(score))
 
 
         
