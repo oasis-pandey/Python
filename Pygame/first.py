23f5abe0567a4c4c172a1fd728fa2eea58f6f2de
hngrmn <143054580+hngrmn@users.noreply.github.com>
2024-02-01 15:04:41 +0545

    created background as white

--- Pygame/first.py
+++ Pygame/first.py
@@ -2,25 +2,16 @@ import pygame
 
 pygame.init() #initializing pygame module
 
-pygame.display.set_mode((1200,500)) #creating a game window
+game_window=pygame.display.set_mode((900, 600)) #creating a game window
 pygame.display.set_caption("My First Game") #title for the window
+game_window.fill((255, 255, 255))
+pygame.display.update()
 
 #game specific variables
 game_over=False
 game_exit=False
 
-# while game_exit==False:
-#     for event in pygame.event.get():
-#         if event.type==pygame.QUIT:
-#             game_exit=True
-
-#         if event.type==pygame.KEYDOWN:
-#             if event.key==pygame.K_RIGHT:
-#                 print("You pressed right key!")
-# pygame.quit()
-# quit()
-
-while game_exit==False:
+while game_exit==False:  
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_exit=True
